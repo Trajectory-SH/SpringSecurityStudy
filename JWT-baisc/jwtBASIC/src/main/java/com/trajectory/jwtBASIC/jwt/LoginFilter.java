25d7404665583664ced8aa2fc1373fa5fe0fcd0d
Trajectory-SH <trajectory0611@gmail.com>
2025-08-23 10:16:10 +0900

    Spring Security

--- JWT-baisc/jwtBASIC/src/main/java/com/trajectory/jwtBASIC/jwt/LoginFilter.java
+++ JWT-baisc/jwtBASIC/src/main/java/com/trajectory/jwtBASIC/jwt/LoginFilter.java
@@ -1,5 +1,6 @@
 package com.trajectory.jwtBASIC.jwt;
 
+import com.trajectory.jwtBASIC.dto.CustomUserDetails;
 import io.jsonwebtoken.Jwts;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
@@ -10,9 +11,12 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import java.io.IOException;
+import java.util.Collection;
+import java.util.Iterator;
 
 @RequiredArgsConstructor
 public class LoginFilter extends UsernamePasswordAuthenticationFilter {
@@ -41,11 +45,23 @@ public class LoginFilter extends UsernamePasswordAuthenticationFilter {
 
     @Override
     protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authentication) throws IOException, ServletException {
-        System.out.println("success");
+
+        CustomUserDetails customUserDetails = (CustomUserDetails) authentication.getPrincipal();
+        String username = customUserDetails.getUsername();
+
+        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+        Iterator<? extends GrantedAuthority> iterator = authorities.iterator();
+        GrantedAuthority auth = iterator.next();
+        String role = auth.getAuthority();
+
+        String token = jwtUtil.createJwt(username, role, 60 * 60 * 10L);
+        response.addHeader("Authorization", "Bearer " + token);
+
     }
 
     @Override
     protected void unsuccessfulAuthentication(HttpServletRequest request, HttpServletResponse response, AuthenticationException failed) throws IOException, ServletException {
-        System.out.println("fail");
+        response.setStatus(401);
+
     }
 }
